4e5e2419dddde8893f1147dbf881e1f8be35a5f9
Samyar <samyar1380.work@gmail.com>
2025-01-18 15:35:42 +0330

    update tutorial directory

---
+++ MVCTutorialProject/MVCTutorialProject/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCTutorialProject
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "RegisterStudents",
+                url: "student/register/",
+                defaults: new { controller = "Student", action = "Register" }
+            );
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVCTutorialProject/MVCTutorialProject/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using DataLayer;
+using MVCTutorialProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorialProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDBEntities db = new StudentDBEntities();
+        
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View("CustomTemplate");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            var Result = from S in db.Student 
+                         where S.FirstName == "Kambiz"
+                         select S;
+
+            return View(Result);
+        }
+
+        //[HttpPost]
+        //public ActionResult Register(Student student)
+        //{
+        //    return RedirectToAction("Register");
+        //}
+    }
+}
